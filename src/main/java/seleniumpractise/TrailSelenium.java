ce3f40c6068667d035013149160c6c4626bc1ec5
NaveenSCode <naveen.sekharamahanthi@gmail.com>
2021-06-06 17:15:11 +0530

    code commit 1

---
+++ src/main/java/seleniumpractise/TrailSelenium.java
@@ -0,0 +1,29 @@
+package seleniumpractise;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TrailSelenium {
+	//System.setProperty();
+//	public static void main(String[] args) {
+//		
+//		//set driver
+//		//System.setProperty("webdriver.chrome.driver", "/home/naveen/softwares/Selenium_requirements/chromedriver");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://google.com");
+//		System.out.println("success");
+//	}
+	
+	@Test
+	public void initiateBrowser() {
+		System.setProperty("webdriver.chrome.driver", "/home/naveen/softwares/Selenium_requirements/chromedriver");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		driver.close();
+		System.out.println("success");
+	}
+	
+
+}
